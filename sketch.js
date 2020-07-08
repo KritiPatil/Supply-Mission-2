74be9660a6ce8f8c01a170e3bbd0218af0f5bb96
KritiPatil <66488628+KritiPatil@users.noreply.github.com>
2020-07-08 14:30:46 -0900

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -19,25 +19,21 @@ function setup() {
 
 	packageSprite=createSprite(width/2, 80, 10,10);
 	packageSprite.addImage(packageIMG)
-	packageSprite.scale=0.2
+	packageSprite.scale=0.2;
 
 	helicopterSprite=createSprite(width/2, 200, 10,10);
 	helicopterSprite.addImage(helicopterIMG)
 	helicopterSprite.scale=0.6
 
-	groundSprite=createSprite(width/2, height-35, width,10);
-	groundSprite.shapeColor=color(255)
-
-
 	engine = Engine.create();
 	world = engine.world;
 
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.15, isStatic:true});
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {isStatic:true});
 	World.add(world, packageBody);
 	
 
 	//Create a Ground
-	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+	ground = Bodies.rectangle(width/2, 680, 800, 10, {isStatic:true} );
  	World.add(world, ground);
 
 
@@ -57,8 +53,6 @@ function draw() {
   background(0);
 
   Engine.update(engine);
-
-  packageSprite.collide(box1);
 
   packageSprite.x= packageBody.position.x 
   packageSprite.y= packageBody.position.y 
